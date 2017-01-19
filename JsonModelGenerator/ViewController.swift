6ccf78b11d1d48fbcc92f33db0a115579e92431d
Charles Oder <charles.oder@dtn.com>
2017-01-19 17:00:53 -0600

    Added initial UI

--- JsonModelGenerator/ViewController.swift
+++ JsonModelGenerator/ViewController.swift
@@ -10,9 +10,15 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet var jsonTextView: NSTextView!
+    @IBOutlet weak var directoryField: NSTextField!
+    @IBOutlet weak var suffixField: NSTextField!
+    @IBOutlet weak var prefixField: NSTextField!
+    @IBOutlet weak var baseClassNameField: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        jsonTextView.isAutomaticQuoteSubstitutionEnabled = false
+        
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +28,67 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func generateButtonClicked(_ sender: Any) {
+        guard let text = jsonTextView.string, !text.isEmpty else {
+            print("No JSON")
+            showError(title: "Error", message: "Please paste json sample in the text box.")
+            return
+        }
+        let json = text
+
+        guard let data = json.data(using: .utf8, allowLossyConversion: true) else {
+            print("NO DATA: \(text)")
+            showError(title: "Error", message: "Please enter valid JSON.")
+            return
+        }
+
+        var dict: [String: Any?]?
+        do {
+            dict = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any?]
+        } catch  {
+            showError(title: "Error", message: "Please enter valid JSON.")
+            return
+        }
+        
+        guard !directoryField.stringValue.isEmpty else {
+            showError(title: "Error", message: "Please enter a destination directory.")
+            return
+        }
+        
+        guard !baseClassNameField.stringValue.isEmpty else {
+            showError(title: "Error", message: "Please enter a base class name.")
+            return
+        }
+        
+        let desc = dictionaryProperties(dict: dict!)
+        
+        let location = directoryField.stringValue + prefixField.stringValue + baseClassNameField.stringValue + suffixField.stringValue + ".swift"
+        
+        do {
+            try desc.data(using: .utf8, allowLossyConversion: true)?.write(to: URL(fileURLWithPath: location))
+        } catch {
+            showError(title: "Error", message: "Could not write file: \(location)")
+
+        }
+        
+    }
+    
+    func showError(title: String, message: String) {
+        let alert = NSAlert()
+        alert.alertStyle = .critical
+        alert.messageText = title
+        alert.informativeText = message
+        alert.addButton(withTitle: "OK")
+        alert.runModal()
+    }
+    
+    func dictionaryProperties(dict: [String: Any?]) -> String {
+        var string = ""
+        for (key, val) in dict {
+            string += "\(key):\(String(describing: type(of: val!)))\r"
+        }
+        return string
+    }
 
 }
 
