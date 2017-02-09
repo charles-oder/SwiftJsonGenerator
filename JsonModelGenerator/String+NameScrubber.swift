559e962b6ae578c749f8b4c3cd973fb0a3edb560
Charles Oder <charles.oder@dtn.com>
2017-02-09 08:23:01 -0600

    Added plural cases

--- JsonModelGenerator/String+NameScrubber.swift
+++ JsonModelGenerator/String+NameScrubber.swift
@@ -40,6 +40,45 @@ extension String {
     }
     
     var removePlural: String {
+        if lowercased().hasSuffix("news") {
+            return self
+        }
+        if lowercased().hasSuffix("species") {
+            return self
+        }
+        if lowercased().hasSuffix("series") {
+            return self
+        }
+        if lowercased().hasSuffix("alumnae") {
+            return String(characters.dropLast(2)) + "a"
+        }
+        if lowercased().hasSuffix("geese") {
+            return String(characters.dropLast(4)) + "oose"
+        }
+        if lowercased().hasSuffix("children") {
+            return String(characters.dropLast(3))
+        }
+        if lowercased().hasSuffix("men") {
+            return String(characters.dropLast(2)) + "an"
+        }
+        if lowercased().hasSuffix("people") {
+            return String(characters.dropLast(4)) + "rson"
+        }
+        if lowercased().hasSuffix("radii") {
+            return String(characters.dropLast()) + "us"
+        }
+        if lowercased().hasSuffix("mice") {
+            return String(characters.dropLast(3)) + "ouse"
+        }
+        if hasSuffix("ses") {
+            return String(characters.dropLast(3)) + "sis"
+        }
+        if hasSuffix("ies") {
+            return String(characters.dropLast(3)) + "y"
+        }
+        if hasSuffix("es") {
+            return String(characters.dropLast(2))
+        }
         if hasSuffix("s") {
             return String(characters.dropLast())
         }
--- JsonModelGeneratorTests/String+NameScrubberTests.swift
+++ JsonModelGeneratorTests/String+NameScrubberTests.swift
@@ -36,14 +36,78 @@ class String_NameScrubberTests: XCTestCase {
     }
     
     func testRemovePluralForPluralName() {
-        let testObject = "testObjects".removePlural
-        
-        XCTAssertEqual("testObject", testObject)
+        XCTAssertEqual("testObject", "testObjects".removePlural)
+        XCTAssertEqual("testNews", "testNews".removePlural)
+        XCTAssertEqual("testAnalysis", "testAnalyses".removePlural)
+        XCTAssertEqual("testDiagnosis", "testDiagnoses".removePlural)
+        XCTAssertEqual("testParenthesis", "testParentheses".removePlural)
+        XCTAssertEqual("testPrognosis", "testPrognoses".removePlural)
+        XCTAssertEqual("testSynopsis", "testSynopses".removePlural)
+        XCTAssertEqual("testThesis", "testTheses".removePlural)
+        XCTAssertEqual("testAnalysis", "testAnalyses".removePlural)
+        XCTAssertEqual("testJealousy", "testJealousies".removePlural)
+        XCTAssertEqual("testMouse", "testMice".removePlural)
+        XCTAssertEqual("testPerson", "testPeople".removePlural)
+        XCTAssertEqual("testMan", "testMen".removePlural)
+        XCTAssertEqual("testChild", "testChildren".removePlural)
+        XCTAssertEqual("testSex", "testSexes".removePlural)
+        XCTAssertEqual("testGoose", "testGeese".removePlural)
+        XCTAssertEqual("testAlumna", "testAlumnae".removePlural)
+        XCTAssertEqual("testSpecies", "testSpecies".removePlural)
+        XCTAssertEqual("testEquipment", "testEquipment".removePlural)
+        XCTAssertEqual("testInformation", "testInformation".removePlural)
+        XCTAssertEqual("testRice", "testRice".removePlural)
+        XCTAssertEqual("testMoney", "testMoney".removePlural)
+        XCTAssertEqual("testSeries", "testSeries".removePlural)
+        XCTAssertEqual("testFish", "testFish".removePlural)
+        XCTAssertEqual("testSheep", "testSheep".removePlural)
+        XCTAssertEqual("testDeer", "testDeer".removePlural)
+        XCTAssertEqual("testAircraft", "testAircraft".removePlural)
+        XCTAssertEqual("testRadius", "testRadii".removePlural)
     }
     
-    func testRemovePluralForNonPluralName() {
+    func testRemovePluralForNonPluralNameEndingInS() {
         let testObject = "testObject".removePlural
         
         XCTAssertEqual("testObject", testObject)
     }
+    
+    
+    
+    /*
+     
+     AddSingular("((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)ses$", "$1$2sis");
+     AddSingular("(^analy)ses$", "$1sis");
+     AddSingular("([^f])ves$", "$1fe");
+     AddSingular("(hive)s$", "$1");
+     AddSingular("(tive)s$", "$1");
+     AddSingular("([lr])ves$", "$1f");
+     AddSingular("([^aeiouy]|qu)ies$", "$1y");
+     AddSingular("(s)eries$", "$1eries");
+     AddSingular("(m)ovies$", "$1ovie");
+     AddSingular("(x|ch|ss|sh)es$", "$1");
+     AddSingular("([m|l])ice$", "$1ouse");
+     AddSingular("(bus)es$", "$1");
+     AddSingular("(o)es$", "$1");
+     AddSingular("(shoe)s$", "$1");
+     AddSingular("(cris|ax|test)es$", "$1is");
+     AddSingular("(octop|vir|alumn|fung)i$", "$1us");
+     AddSingular("(alias|status)es$", "$1");
+     AddSingular("^(ox)en", "$1");
+     AddSingular("(vert|ind)ices$", "$1ex");
+     AddSingular("(matr)ices$", "$1ix");
+     AddSingular("(quiz)zes$", "$1");
+     
+     AddUncountable("equipment");
+     AddUncountable("information");
+     AddUncountable("rice");
+     AddUncountable("money");
+     AddUncountable("species");
+     AddUncountable("series");
+     AddUncountable("fish");
+     AddUncountable("sheep");
+     AddUncountable("deer");
+     AddUncountable("aircraft");
+
+ */
 }
